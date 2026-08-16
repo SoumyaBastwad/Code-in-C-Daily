fc0bd2284de0d948e0700aac3939027abfd33e24
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-08-16 23:04:04 +0530

    Update secondlargest.c

--- secondlargest.c
+++ secondlargest.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
+
 int main(){
-  int arr[5]={10,25,30,5,6]
-  int large=arr[0];
+  int arr[5]={10,25,30,5,6};
+  int largest=arr[0];
   int second=arr[0];
-for(i=1:i<=5:i++){
-if(arr[i]>large){
-large=arr[i];
-second=large;
+for(int i=1;i<5;i++){
+if(arr[i]> largest){
+second=largest;
+largest=arr[i];
+
 }
-else if (arr[i]>second && arr[i]!=large){
+else if (arr[i]>second && arr[i] != largest){
   second=arr[i];
 }
 }
 printf("second largest number is :%d",second);
+return 0;
+
 }
