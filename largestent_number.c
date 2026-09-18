9db59be7525f2fb1b8243ceebd1d19bb42f1319b
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-18 22:18:05 +0530

    Update largestent_number.c

--- largestent_number.c
+++ largestent_number.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+void main(){
+  int a,b,c;
+printf("enter the 3 numbers:",);
+scanf("%d %d %d",&a,&b,&c);
+if(a>b && a>c){
+printf("a is larger number that is %d",a);
+}
+if(b>a && b>c){
+printf("b is larger number that is %d",b);
+}
+else{
+printf("c is larger number that is %d ",c);
+}
+}
