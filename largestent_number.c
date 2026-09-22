1ebcc8e13742b517dc61c7cf93d728432d2589bb
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-22 23:26:58 +0530

    Update largestent_number.c

--- largestent_number.c
+++ largestent_number.c
@@ -8,9 +8,4 @@ if(a>b && a>c){
      printf("a is larger number that is %d",a);
   }
 if(b>a && b>c){
-    printf("b is larger number that is %d",b);
- }
-else{
-printf("c is larger number that is %d ",c);
-}
-}
+    
