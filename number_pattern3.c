d386c46c48c71c173ca751fa613b16f468a6e767
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-23 21:36:40 +0530

    Update number_pattern3.c

--- number_pattern3.c
+++ number_pattern3.c
@@ -8,13 +8,7 @@
    scanf("%d",&n);
    for(i=1;i<=n;i++)
    {
-     for(j=1;j<=i;j++)
-      {
-         printf("%d",j);
-      }
-      printf("\n");
-   }
- }
+ 
 // number pattern code in c
 
 #include<stdio.h>
