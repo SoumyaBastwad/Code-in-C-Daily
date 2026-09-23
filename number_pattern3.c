ad692cd32b3aced9effdbe5e634b8235949ad8dc
Soumya Bastwad <soumyabastwad874@gmail.com>
2026-09-23 21:36:58 +0530

    Update number_pattern3.c

--- number_pattern3.c
+++ number_pattern3.c
@@ -1,13 +1,7 @@
 // number pattern code in c
 
 #include<stdio.h>
- void main()
- {
-   int i,j,n;
-   printf("enter the number of rows:");
-   scanf("%d",&n);
-   for(i=1;i<=n;i++)
-   {
+ void main(
  
 // number pattern code in c
 
